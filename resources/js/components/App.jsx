7b5a6be9d99dd990c8ada3d01936785d410d1446
ZweMinOo <zweminoo@siriertech.com>
2024-06-14 22:33:38 +0630

    Initialization Project: Install laravel 11 and react

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <center>
+            <h1>Strat Explore Project Tracker</h1>
+        </center>
+    );
+}
+
+export default App;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Project Tracker</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+</head>
+
+<body>
+    <div id="root"></div>
+</body>
+
+</html>
